8be4c0d7a490b07b8909c32f67b6a77c02189ee9
stefhan235 <stefhansalim12@gmail.com>
2024-05-25 11:28:57 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'tutorial_12.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+      routes: {
+        '/tutorial_12': (context) => Tutorial12(),
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/tutorial_12');
+          },
+          child: Text('PRODUCT LIST'),
+        ),
+      ),
+    );
+  }
+}
